6e79eeffc0de9471c6b02e763b6bb85ba6c48cd6
ft3ix3iR4 <fellipewolly2@gmail.com>
2024-09-06 21:19:39 -0300

    fix parameters

--- parameters.tf
+++ parameters.tf
@@ -1,11 +1,11 @@
 resource "aws_ssm_parameter" "lb_arn" {
   name  = "/linutxtips/ecs/lb/id"
   value = aws_lb.main.arn
-  type  = "string"
+  type  = "String"
 }
 
 resource "aws_ssm_parameter" "lb_listener" {
   name  = "/linutxtips/ecs/lb/listener"
   value = aws_lb_listener.main.arn
-  type  = "string"
+  type  = "String"
 }
