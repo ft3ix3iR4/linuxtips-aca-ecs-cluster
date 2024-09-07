b2d53eff536a5fe45b5aaf13cde6edcc1959b6bf
ft3ix3iR4 <fellipewolly2@gmail.com>
2024-09-06 21:21:07 -0300

    fix sg

--- security_group.tf
+++ security_group.tf
@@ -16,7 +16,7 @@ resource "aws_security_group_rule" "subnet_ranges" {
   type              = "ingress"
   from_port         = 0
   to_port           = 0
-  description       = "Liberando tráfego para a VPC."
+  description       = "Liberando trafego para a VPC."
   protocol          = "-1"
   cidr_blocks       = ["10.0.0.0/16"]
   security_group_id = aws_security_group.main.id
