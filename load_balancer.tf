583e19859a1c10ea1d0d0a9f11e9d3710adc4143
ft3ix3iR4 <fellipewolly2@gmail.com>
2024-09-06 21:46:12 -0300

    fix load_balancer.tf

--- load_balancer.tf
+++ load_balancer.tf
@@ -38,9 +38,9 @@ resource "aws_lb" "main" {
   load_balancer_type = var.load_balancer_type
 
   subnets = [
-    data.aws_ssm_parameter.subnet_public_1a.value,
-    data.aws_ssm_parameter.subnet_public_1b.value,
-    data.aws_ssm_parameter.subnet_public_1c.value
+    data.aws_ssm_parameter.public_subnet_1a.value,
+    data.aws_ssm_parameter.public_subnet_1b.value,
+    data.aws_ssm_parameter.public_subnet_1c.value
   ]
 
   security_groups = [
